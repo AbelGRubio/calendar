9948f305b92f0c6fdf5430ca500cdeebaf4988bb
Abel García <46522725+AbelGRubio@users.noreply.github.com>
2025-08-26 09:47:23 +0200

    fit(Abel): update url axio

--- src/components/axio.interceptor.js
+++ src/components/axio.interceptor.js
@@ -11,7 +11,7 @@ import { toast } from "sonner";
  * Axios instance used for making authenticated HTTP requests to the backend API.
  */
 const api = axios.create({
-  baseURL: "https://fastapi-agr.vercel.app/api/py",
+  baseURL: "https://cb-agr.vercel.app/v1",
   //baseURL: "http://localhost:8000/v1",     
   timeout: 10000,       // Request timeout (10 seconds)
 });
